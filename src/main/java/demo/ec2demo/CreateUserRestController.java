c1c50f4794522f62943db1210cdde4d02e1358b9
linnykoleh <oleh.linnyk@megogo.net>
2024-10-23 11:51:39 +0300

    initial commit

--- src/main/java/demo/ec2demo/CreateUserRestController.java
+++ src/main/java/demo/ec2demo/CreateUserRestController.java
@@ -1,45 +1,20 @@
-package demo.javaspringbootmongodb;
+package demo.ec2demo;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 @RestController
 @RequiredArgsConstructor
 public class CreateUserRestController {
 
-    private final UserService userService;
+    int id = new Random().nextInt(10000);
 
-    @PostMapping("/user/{name}/{lastName}")
-    public String createUser(@PathVariable String name, @PathVariable String lastName) {
-        System.out.println("Creating user: " + name + " " + lastName);
-        return userService.createUser(name, lastName);
-    }
-
-    @GetMapping("/user/{name}")
-    public ResponseEntity<User> getUser(@PathVariable String name) {
-        try {
-            System.out.println("Getting user: " + name);
-            var user = userService.getUser(name);
-            return ResponseEntity.ok(user);
-        } catch (Exception e) {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @DeleteMapping("/user/{name}")
-    public ResponseEntity<String> deleteUser(@PathVariable String name) {
-        try {
-            System.out.println("Deleting user: " + name);
-            userService.getUser(name);
-            return ResponseEntity.ok("Deleted");
-        } catch (Exception e) {
-            return ResponseEntity.notFound().build();
-        }
+    @GetMapping("/")
+    public String home() {
+        return "Hello from " + id;
     }
 
 }
